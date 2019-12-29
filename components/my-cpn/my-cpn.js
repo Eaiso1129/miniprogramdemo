714da8fe93a0d0fe8afa29154e4e0692a90adc1a
zhuzj <1135106650@qq.com>
2019-12-29 18:42:42 +0800

    组件事件外传

--- components/my-cpn/my-cpn.js
+++ components/my-cpn/my-cpn.js
@@ -4,8 +4,20 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    title: String, // 属性定义方式一
+    content: { // 方式二
+      type: String, // 属性类型
+      value: '默认内容', // 默认值
+      observer: function(newVal,oldVal){ // 属性值观察者
+        console.log(newVal,oldVal)
+      }
+    }
   },
+
+  /**
+   * 样式枚举，相当于为在页面指定样式提供样式名称的枚举，这些样式都已定义在页面的样式文件中
+   */
+  externalClasses: ['titleclass'],
 
   /**
    * 组件的初始数据
@@ -18,6 +30,9 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    handleIncrement() {
+      // console.log('---------')
+      this.triggerEvent('increment',{name: 'eaiso',age: 19},{})
+    }
   }
 })
